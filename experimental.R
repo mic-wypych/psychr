81ffc7ca30935a496ee720cc45a8b3cfdee9b2c3
MWypych <wypych.mic@gmail.com>
2023-09-14 15:27:22 +0200

    fix base R line plot in class 9

--- experimental.R
+++ experimental.R
@@ -45,3 +45,25 @@ glimpse(midwest)
 
 
 table(midwest$state)
+
+
+midwest %>%
+  group_by(state)%>%
+  reframe(q_10 = quantile(midwest$area, c(.1, .5, .9))) %>%
+  
+
+
+quantile(midwest[which(midwest$state == "IN"),]$area, c(.1,.5,.9))
+
+
+
+list_q <- tapply(midwest$percadultpoverty, midwest$state, quantile, c(.1,.5,.9))
+
+df_q <- data.frame()
+df_q <- bind_rows(list_q[[1]], list_q[[2]], list_q[[3]], list_q[[4]], list_q[[5]])
+colnames(df_q) <- c("q_10", "q_50", "q_90")
+
+plot(1:5, df_q$q_10, ylim = c(0,20), type = "l", col = 2, lwd = 2, lty = 2)
+lines(1:5, df_q$q_50, col = 4, lwd = 2, lty = 1)
+lines(1:5, df_q$q_90, col = 2, lwd = 2, lty = 2)
+axis(side = 3, at = 1:5, labels = unique(midwest$state))
